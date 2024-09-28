f12b50726054ebcd7c02e6d8f075a353bc5401de
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-09-28 13:41:58 -0700

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="text-center text-cyan-500 bg-gray-300 p-4">
+        Hello Vite <img src={viteLogo} alt="Vite Logo" className="h-4 inline" /> + <img src={reactLogo} alt="React Logo" className="h-4 inline" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="text-center">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
