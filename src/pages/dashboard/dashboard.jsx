c1066a552427d2845ae1a0ac57c239c57bf55496
Manas Bhambhra <dellanbhambhra@gmail.com>
2024-10-19 17:04:18 +0530

    blue

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -390,7 +390,7 @@ const Dashboard = () => {
                     )}
                     <button
                         onClick={() => navigate("/create")}
-                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-4 transition duration-150 ease-in-out"
+                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-customBlue hover:bg-customBlueDark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-4 transition duration-150 ease-in-out"
                     >
                         Create New Quiz
                     </button>
